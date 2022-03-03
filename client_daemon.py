43c4431fe641dec3f1d45f1b48c1eff49613386a
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-03 18:26:53 +0300

    add logging

--- client_daemon.py
+++ client_daemon.py
@@ -6,58 +6,58 @@ from daemon import daemon
 
 class ClientDaemon(daemon):
         logging_level = 30 
-        log = logging.basicConfig(level = logging.DEBUG, filename = '/tmp/client_daemon.log', filemode='w')
+        # log = logging.basicConfig(level = logging.DEBUG, filename = '/tmp/client_daemon.log', filemode='w')
 
         def run(self):
             # self.connect()
             print('daemon running')
 
-        def connect(self):
-            HOST = "" 
-            PORT = 5432  # Port to listen on (non-privileged ports are > 1023)
+        # def connect(self):
+        #     HOST = "" 
+        #     PORT = 5432  # Port to listen on (non-privileged ports are > 1023)
 
-            try:
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.bind((HOST, PORT))
-                    s.listen()
-                    print(('daemon started'))
-                    self.log.DEBUG('daemon listening on port '+ str(PORT))
+        #     try:
+        #         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #             s.bind((HOST, PORT))
+        #             s.listen()
+        #             print(('daemon started'))
+        #             self.log.DEBUG('daemon listening on port '+ str(PORT))
 
-                    while True:
-                        conn, addr = s.accept()
-                        with conn:
-                            print(f"Connected by {addr}")
-                            while True:
-                                data = conn.recv(1024)
-                                if not data:
-                                    break
+        #             while True:
+        #                 conn, addr = s.accept()
+        #                 with conn:
+        #                     print(f"Connected by {addr}")
+        #                     while True:
+        #                         data = conn.recv(1024)
+        #                         if not data:
+        #                             break
                                 
-                                data = data.decode("utf-8")
+        #                         data = data.decode("utf-8")
 
-                                self.log.DEBUG('received the following:')
-                                self.log.DEBUG(data)
+        #                         self.log.DEBUG('received the following:')
+        #                         self.log.DEBUG(data)
 
-                                self.execute(data)
+        #                         self.execute(data)
 
-                                replay = { "message": "data received"}
-                                replay = json.dumps(replay)
-                                conn.sendall(bytes(replay,encoding="utf-8"))
-            except Exception as e:
-                print(e)
-                self.log.DEBUG(e)
+        #                         replay = { "message": "data received"}
+        #                         replay = json.dumps(replay)
+        #                         conn.sendall(bytes(replay,encoding="utf-8"))
+        #     except Exception as e:
+        #         print(e)
+        #         self.log.DEBUG(e)
 
 
-            finally:
-                s.close()
-                return data
+        #     finally:
+        #         s.close()
+        #         return data
         
-        def execute(self, json_command):
-            file = json_command["script_path"]
-            try:
-                os.system(file)
-            except Exception as e:
-                self.log.DEBUG(e)
-            return True
+        # def execute(self, json_command):
+        #     file = json_command["script_path"]
+        #     try:
+        #         os.system(file)
+        #     except Exception as e:
+        #         self.log.DEBUG(e)
+        #     return True
  
 if __name__ == "__main__":
 
@@ -68,7 +68,7 @@ if __name__ == "__main__":
                 if 'start' == sys.argv[1]:
                         print('daemon started')
                         daemon.start()
-                        daemon.log.DEBUG('daemon started')
+                        # daemon.log.DEBUG('daemon started')
                 elif 'stop' == sys.argv[1]:
                         daemon.stop()
                 elif 'restart' == sys.argv[1]:
