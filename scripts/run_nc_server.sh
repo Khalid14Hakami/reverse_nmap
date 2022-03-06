db58083d18c55e150e28019690b3f3d1544223bf
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-06 03:03:34 +0300

    fixed scripts

--- scripts/run_nc_server.sh
+++ scripts/run_nc_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 iptables -F
-dst_ip =`ping -c1 c1 | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'`
+dst_ip=`ping -c1 c1 | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'`
 iptables -A OUTPUT -d $dst_ip -p tcp --tcp-flags SYN,ACK SYN,ACK -j DROP 
 nc -l 2000
