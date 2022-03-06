ac777db62100b701ca3dda49fc5a34bbe6b662ae
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-06 04:14:43 +0300

    adding sniffing scripts

---
+++ scripts/sniffer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+
+#start a process in the background (it happens to be a TCP HTTP sniffer on  the loopback interface, for my apache server):   
+
+tcpdump -U -w test_capture.pcap host c1 or host s1& 
+sleep 5
+
+# interrupt the sniffer.  get its PID:  
+pid=$(ps -e | pgrep tcpdump)  
+echo $pid  
+
+#interrupt it:  
+sleep 180s
+kill -2 $pid
