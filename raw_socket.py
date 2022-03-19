c2c875266c6f2f13950da2c1e970996c9554cd0a
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-19 20:41:14 +0300

    add raw_socket.py

--- raw_socket.py
+++ raw_socket.py
@@ -1,6 +1,11 @@
 import socket
+from scapy.all import *
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 s.bind(('0.0.0.0', 1337))
 while True:
-    packet = s.recvfrom(65535)[0].decode()    #decode packet
-    print(packet)   #print packet to read
+    # packet = s.recvfrom(65535)[0].decode()    #decode packet
+    # print(packet)   #print packet to read
+    packet = s.recvfrom(2000)
+    packet = packet[0]
+    ip = IP(packet)
+    ip.show()
