3d3d2ec95d14a20a9c3f2229e0f381a1b9af00a9
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-19 20:36:54 +0300

    add raw_socket.py

--- raw_socket.py
+++ raw_socket.py
@@ -2,5 +2,5 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 s.bind(('0.0.0.0', 1337))
 while True:
-    packet = s.recvfrom(65535).decode()    #decode packet
+    packet = s.recvfrom(65535)[0].decode()    #decode packet
     print(packet)   #print packet to read
