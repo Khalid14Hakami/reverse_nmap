a66a39c39eced2520d0ee7311a86a00d51d51559
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-21 15:46:22 +0300

    raw socket update

--- raw_socket.py
+++ raw_socket.py
@@ -9,9 +9,12 @@ while True:
     packet = mystream.recv(2000)
     print('this what we got:')
     print("".join(map(chr, bytes(packet[0]))))
-    p = packet
+    p = packet[0]
     print(type(p))
     # p = IP(packet)
+    if Raw in packet:
+        load = packet[Raw].load
+        print(load)
     print(packet.summary())
     if True: # p['TCP'].flags == 'S':
         ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
