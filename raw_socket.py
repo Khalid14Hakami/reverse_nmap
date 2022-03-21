8b705be157177899530ee9ea73be38346dc3d1e5
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-21 15:31:44 +0300

    raw socket update

--- raw_socket.py
+++ raw_socket.py
@@ -2,10 +2,11 @@ import socket
 from scapy.all import *
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 s.bind(('0.0.0.0', 1337))
+mystream=StreamSocket(mysocket)
 while True:
     # packet = s.recvfrom(65535)[0].decode()    #decode packet
     # print(packet)   #print packet to read
-    packet = s.recvfrom(2000)
+    packet = mystream.recv(2000)
     packet = packet[0]
     p = IP(packet)
     if True: # p['TCP'].flags == 'S':
@@ -18,14 +19,6 @@ while True:
         )
         pair, unans = sr(ip, verbose=30)
         print (pair)
-        # if len(pair) != 1:
-        #     print("We have received %s answers to the SYN pckt instead of 1.\nAborting", len(pair))
-        #     exit()
-        # if str(pair[0][1][TCP].flags)=="SA":
-        #     print("SYN ACK packet received")
-        #     synackpkt=pair[0][1]
-        # else:
-        #     print("Response to the SYN Packet had the %s flags instead of SYN ACK\n===>You probably forgot to launch your server. By default it should be on s3.net3.local, port 2000\n\"nc -l 2000\"\n\nAborting", str(pair[0][1][TCP].flags))
-        #     exit()
+    
 
     print ("%s\n" % (p[IP].summary()))
