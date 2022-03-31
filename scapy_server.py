d5d0e33e4f4d09f9f9617cc50489c5120f7f9ef8
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-03-31 22:27:44 +0300

    scapy server

--- scapy_server.py
+++ scapy_server.py
@@ -18,11 +18,11 @@ while True:
     print('this what we got:')
     print("".join(map(chr, bytes(packet[0]))))
     p = packet[0]
-    print(type(p))
+    print(type(packet))
     # p = IP(packet)
-    if Raw in packet:
-        load = packet[Raw].load
-        print(load)
+    # if Raw in packet:
+    #     load = packet[Raw].load
+    #     print(load)
     print(packet.summary())
     if True: # p['TCP'].flags == 'S':
         ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
