09ac7796a86126c151906ab0a868a20e8ec318f6
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-02 22:35:35 +0300

    added scappy server

--- scapy_server.py
+++ scapy_server.py
@@ -7,9 +7,18 @@ s.bind(('0.0.0.0', 1337))
 test_istruction= {
     "condition": "p['TCP'].dport == 80",
     "steps": [
-        """print('this is a test', p['TCP'].dport)
+        """
+print('this is a test', p['TCP'].dport)
+ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
+flags='SA',
+sport=p['TCP'].dport,
+dport=p['TCP'].sport,
+seq = 0,
+ack = p['TCP'].seq + 1,
+)
 pair = sr1(ip, verbose=30)"""
-    ]
+    ], 
+    "states": ("p['TCP'].flags == 'S'", [1, 2])
 }
 
 while True:
@@ -26,17 +35,10 @@ while True:
     #     print(load)
     print(p.summary())
     if True: # p['TCP'].flags == 'S':
-        ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
-        flags='SA',
-        sport=p['TCP'].dport,
-        dport=p['TCP'].sport,
-        seq = 0,
-        ack = p['TCP'].seq + 1,
-        )
+        for state in test_istruction["states"]:
+            if eval(state[0]):
+                for step in state[1]:
+                    exec(test_istruction["steps"][step])
 
-        exec(test_istruction['steps'][0])
-        # pair, unans = sr1(ip, verbose=30)
-        # print (pair)
-    
 
-    print ("%s\n" % (p[IP].summary()))
+            print ("%s\n" % (p[IP].summary()))
