cceca68cd058f9565420c3a359a76baca31b5ce6
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-03 22:33:08 +0300

    server daemon auto registration

--- client_daemon.py
+++ client_daemon.py
@@ -4,6 +4,8 @@ import sys, socket, json, logging, os
 from time import sleep
 from daemon import daemon
 import subprocess
+import ast
+from scapy.all import *
 
  
 
@@ -56,15 +58,81 @@ class ClientDaemon(daemon):
         finally:
             s.close()
             return data
+
+    def launch_Server(test_istruction):
+        
+
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        s.bind(('0.0.0.0', 1337))
+        # mystream=StreamSocket(s)
+
+        # test_istruction= {
+        #     "condition": "p['TCP'].dport == 80",
+        #     "steps": [
+        #         """
+        # print('this is a test', p['TCP'].dport)
+        # ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
+        # flags='SA',
+        # sport=p['TCP'].dport,
+        # dport=p['TCP'].sport,
+        # seq = 0,
+        # ack = p['TCP'].seq + 1,
+        # )
+        # pair = sr1(ip, verbose=30)""",
+        # """
+        # print('this is a step 2', p['TCP'].dport)
+        # ip = IP(src=p['IP'].dst, dst=p['IP'].src)/TCP(
+        # flags='A',
+        # sport=p['TCP'].dport,
+        # dport=p['TCP'].sport,
+        # seq = p['TCP'].seq ,
+        # ack = p['TCP'].seq + p['TCP'].len,
+        # )
+        # pair = sr1(ip, verbose=30, timeout= 3)"""
+        #     ], 
+        #     "states": [
+        #             ("p['TCP'].flags == 'S'", "[0]"), 
+        #             ]
+        # }
+
+        while True:
+            # packet = s.recvfrom(65535)[0].decode()    #decode packet
+            # print(packet)   #print packet to read
+            packet = s.recv(2000)
+            print('this what we got:')
+            print("".join(map(chr, bytes(packet[0]))))
+            p = packet[0]
+            print(type(packet))
+            p = IP(packet)
+            # if Raw in packet:
+            #     load = packet[Raw].load
+            #     print(load)
+            print(p.summary())
+            if True: # p['TCP'].flags == 'S':
+                for state in test_istruction["states"]:
+                    if eval(state[0]):
+                        for step in ast.literal_eval(state[1]):
+                            print(step)
+                            exec(test_istruction["steps"][int(step)])
+
+
+                    print ("%s\n" % (p[IP].summary()))
+        
     
     def execute(self, json_command):
         file = json_command["script_path"]
+        test = json_command["test_istruction"]
+        register = multiprocessing.Process(target=self.launch_server, args=[test])
         try:
-            subprocess.Popen(file)
+            # subprocess.Popen(file)
+            register.start()
+            register.join()
         except Exception as e:
             self.logger.exception(e)
             return(e)
         return True
+
 
     def register(self):
         self.logger.debug("start registration")
--- controller/app.py
+++ controller/app.py
@@ -14,7 +14,72 @@ class MyPrompt(Cmd):
         
 
     def do_run_test(self, inp):
+        if  os.path.isfile(inp) and os.path.splitext(inp)[1] == '.json':
+            # load json from file
+            scenario = self.get_scenario(inp)
+            server_settings = scenario['server_setting']
+            clients_settings = scenario['clients_settings']
+            sniffer_settings = scenario['sniffer_settings']
+
+            server_status = self.prepare_server(server_settings)
+            # snfiffer_status = prepare_sniffer(sniffer_settings)
+
+            if server_status and snfiffer_status:
+                trigger_clients(clients_settings)
+        else:
+            print ("file format not suported")
+            sys.exit(2)
+        sys.exit(0)
         print(reg.get_workers())
+
+
+    def get_scenario(self, scenario_name):
+        """ this function get the scenario configuration from json file by name"""
+        f = open(scenario_name)
+        return json.load(f)
+
+    def prepare_server(self, server_settings):
+        """
+        this function takes the server setting 
+        from the scenario config and prepare server 
+        by contacting the server daemon
+        """
+        result = self.connect(server_settings)
+        if result['message'] == True:
+            return True
+        else:
+            print('server did not start successfully')
+            print(result)
+        # sys.exit(2) 
+
+    def connect(configs):
+        HOST = configs['HOST'] # socket.gethostname() 
+        PORT = 5432  # Port to listen on (non-privileged ports are > 1023)
+        ans = ""
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, PORT))
+                
+
+                print(f"Connected to {HOST}")
+
+                data = {
+                    "script_path": configs['script_path'],
+                    "test_istruction": configs['test_istruction']
+                }
+                data = json.dumps(data)
+                s.sendall(bytes(data,encoding="utf-8"))
+
+                ans = s.recv(1024)
+                ans = ans.decode("utf-8")
+                ans = json.loads(ans)
+                print(ans) 
+        except Exception as e:
+            print(e)       
+        finally:
+            s.close()
+            print ("answer: \n", ans )
+            return ans  
  
 
 
