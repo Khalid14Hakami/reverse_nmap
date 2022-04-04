13ae23251e60e14b28f05b2fde2871f5fb21bbfc
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-04 05:50:13 +0300

    fixed logs in server subporcess

--- client_daemon.py
+++ client_daemon.py
@@ -28,7 +28,7 @@ class ClientDaemon(daemon):
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
                 s.bind((HOST, PORT))
-                s.listen()
+                s.listen(1)
                 print(('daemon started'))
                 self.logger.debug('daemon listening on port '+ str(PORT))
 
