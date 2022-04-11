f3d8c8358fca25c044d8228b24c3dc63e1a27286
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-11 05:50:05 +0300

    added stateful server

--- stateful_thread.py
+++ stateful_thread.py
@@ -2,6 +2,7 @@ import threading
 import time, socket
 from scapy.all import *
 from queue import Queue
+import json
 
 print_lock = threading.Lock()
 
@@ -9,10 +10,13 @@ class StatefulSocket(threading.Thread):
     def __init__(self, queue, socket, args=(), kwargs=None):
         threading.Thread.__init__(self, args=(), kwargs=None)
         self.state = "init"
+        global state_machine
+        f = open('staeful.json')
+        state_machine = json.load(f)
+        f.close()
+        self.states = state_machine.copy()
         self.socket = socket
         self.queue = queue
-        self.daemon = True
-        # self.receive_messages = args[0]
 
     def run(self):
         print (threading.currentThread().getName())
@@ -25,6 +29,10 @@ class StatefulSocket(threading.Thread):
     def do_thing_with_message(self, message):
         with print_lock:
             print (threading.currentThread().getName(), "Received {}".format(message.summary()))
+            for transition in self.states[self.state]["transitions"]:
+                if eval(transition["transiotion_condition"]):
+                    exec(transition["transition_response"])
+                    self.state = transition["next_state"]
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
@@ -41,24 +49,11 @@ if __name__ == '__main__':
             print(type(address))
             p = packet[0]
             p = IP(packet)
-            # if Raw in packet:
-            #     load = packet[Raw].load
-            #     print(load)
+
             print(p.summary())
             client_address = str(p[IP].src) + ":" + str(p[TCP].sport)
             print("got data from: ", client_address)
 
-            # if client_address not in connections.keys():
-            #     q = Queue()
-            #     connections[client_address] = StatefulSocket(q, str)
-            #     connections[client_address].start()
-            # else:
-            #     if connections[client_address].is_alive():
-            #         connections[client_address].queue.put(p)
-            #     else:
-            #         q = Queue()
-            #         connections[client_address] = StatefulSocket(q, str)
-            #         connections[client_address].start()
 
             if client_address in connections.keys() and connections[client_address].is_alive():
                 connections[client_address].queue.put(p)
@@ -68,30 +63,7 @@ if __name__ == '__main__':
                 connections[client_address].start()
                 connections[client_address].queue.put(p)
 
-            # for state in test_istruction["states"]:
-            #     logger.debug(state)
-            #     if eval(state[0]):
-            #         logger.debug(state[0])
-
-            #         for step in state[1]:
-            #             logger.debug(step)
-            #             exec(test_istruction["steps"][int(step)])
-
-
-            #     logger.debug ("%s\n" % (p[IP].summary()))
             counter = counter + 1 
                         
     except Exception as e:
         print(e)
-
-    # for t in range(10):
-    #     q = Queue()
-    #     threads.append(MyThread(q, args=(t % 2 == 0,)))
-    #     threads[t].start()
-    #     time.sleep(0.1)
-
-    # for t in threads:
-    #     t.queue.put("Print this!")
-
-    # for t in threads:
-    #     t.join()
