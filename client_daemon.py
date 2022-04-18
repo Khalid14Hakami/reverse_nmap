726bad86f7051e8e7a39b2e49eae35acfb39cead
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-18 17:18:29 +0300

    fixed thread timeout

--- client_daemon.py
+++ client_daemon.py
@@ -22,7 +22,7 @@ class StatefulSocket(threading.Thread):
         self.timeout = 120 # in seconds
         self.states = server_state_machine
         self.queue = queue
-        self.state_start_time = time.time()
+        self.state_start_time = time.monotonic()
         # logging.basicConfig(level = logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', filename = '/tmp/client_daemon.log', filemode='w')
         self.logger = logging.getLogger('thread-'+threading.currentThread().getName())
 
@@ -50,13 +50,14 @@ class StatefulSocket(threading.Thread):
                     exec(transition["transition_response"])
                     print("from this state "+ self.state + "to "+ transition["next_state"])
                     self.state = transition["next_state"]
-                    self.state_start_time = time.time()
+                    self.state_start_time = time.monotonic()
                     break # dont check other transitions 
     
     def check_timeout(self):
-        self.logger.debug((time.time() - self.state_start_time))
+        self.logger.debug(self.state_start_time)
+        self.logger.debug((time.monotonic() - self.state_start_time))
         self.logger.debug(float(self.states["states"][self.state]["timeout"]))
-        return (time.time() - self.state_start_time) > float(self.states["states"][self.state]["timeout"])
+        return (time.monotonic() - self.state_start_time) > float(self.states["states"][self.state]["timeout"])
 
 class ClientDaemon(daemon):
     logging_level = 30 
