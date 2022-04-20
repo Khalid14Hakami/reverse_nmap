42d0d87d4c2ac69bd7fb51ac29b4714605f84d11
KHALID HAKAMI <khalid.hakami14@gmail.com>
2022-04-20 12:55:28 +0300

    fixed scripts

--- scripts/prep_launch.sh
+++ scripts/prep_launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-chmod 777 client_daemon.py scripts/* iptables.sh kill_client.sh
-./iptables.sh
-./client_daemon.py start 
+chmod 777 daemon/client_daemon.py scripts/* 
+./scriptsiptables.sh
+# ./daemon/client_daemon.py start 
